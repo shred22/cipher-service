4799f391a8e65c4655bbf6fe84f7e9433b96ba12
shred22 <shreyas.dange22@gmail.com>
2020-10-08 21:33:49 +0530

    Initial Draft

---
+++ src/main/java/com/cipher/service/utils/RSAKeyUtils.java
@@ -0,0 +1,27 @@
+package com.cipher.service.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Slf4j
+public class RSAKeyUtils {
+
+    public static PublicKey getPublicKey(String base64PublicKey){
+        PublicKey publicKey = null;
+        try{
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes()));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            publicKey = keyFactory.generatePublic(keySpec);
+            return publicKey;
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return publicKey;
+    }
+}
